706bd97161bcac2dc6b1399139ed835228144439
BaktiyarYerkinov <94916523+BaktiyarYerkinov@users.noreply.github.com>
2023-10-16 16:26:05 +0200

    Changed cors

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
+    origin: ["https://finance-tracker-frontend-dusky.vercel.app/"],
     methods: ["POST", "GET", "PUT", "DELETE"],
     credentials: true,
   })
